bcd31c49fc26f2044214e9946fc1933005afdf93
pedinxdgo <pedinxdgo@hotmail.com>
2026-05-21 12:00:03 -0300

    Site

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-function executarSistema(){
-    //Dados de Entrada
+function executarSistema() {
+    // Dados de entrada
     const nome = document.getElementById("inputNome").value;
     const idade = parseInt(document.getElementById("inputIdade").value);
     const valor = parseFloat(document.getElementById("inputValor").value);
     const cupom = document.getElementById("inputCupom").value === "true";
 
-    //Dados de Saída
+    // Dados de saída
     const msg = document.getElementById("mensagem-autorizacao");
     const lista = document.getElementById("lista-estoque");
     const relatorio = document.getElementById("relatorio-final");
 
-    //Validação para campos vazios
-    if (!nome || isNaN(idade) || isNaN (valor)) {
+    // Validação para campos vazios
+    if (!nome || isNaN(idade) || isNaN(valor)) {
         alert("Por favor, preencha todos os campos!");
         return;
     }
-    // Regra de negocios
+
+    // Regra de negócio
     if (idade >= 16) {
-        msg.innerText = `Venda autotizada: ${nome}`;
-        msg.style.color = "#f0f8ff";
+        msg.innerText = `Venda autorizada: ${nome}`;
+        msg.style.color = "#00ff88";
 
-    // Desconto
-    let calculoTotal = (valor > 500 || cupom ) ? valor * 0.85 : valor;
+        // Desconto
+        let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
 
-    // Estoque
-    let Estoque = ["Placa de Vídeo", "Processador", "Memória RAM"];
-    lista.innerHTML = ""; //Limpa a lista anterior
+        // Estoque
+        let estoque = ["Placa de Vídeo", "Processador", "Memória RAM"];
+        lista.innerHTML = ""; // Limpa a lista anterior
+        
+        //ForEach percorre um array e aplica uma ação para cada elemento
+        estoque.forEach(item => {
+            let li = document.createElement("li");
+            li.innerText = `Item ${item} reservado.`;
+            lista.appendChild(li); //usado para adicionar um novo elemento ou texto
+
+
+        })
+
+        //Relatório
+        relatorio.style.display = "block";
+        relatorio.innerHTML = `
+        <strong> RESUMO DO PEDIDO<\strong><br>
+        Cliente: ${nome} <br>
+        Total Original: R$ ${valor.toFixed(2)} <br>
+        <strong> Total com Desconto: R$ ${valorFinal.toFixed(2)} <\strong>`
+    }
+
+    else{
+        msg.innerText = "Venda bloqueada: Menor de 16 anos.";
+        msg.style.color = "#ff4444";
+        relatorio.style.display = "none"
+        lista.innerHTML = "";
     }
 }
